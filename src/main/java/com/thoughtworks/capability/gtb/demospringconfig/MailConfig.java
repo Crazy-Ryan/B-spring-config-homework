483af1d9a50b80b58db6a8aba3cd652117c80bd7
Crazy Ryan <>
2021-04-30 16:55:33 +0800

    finish problem  1

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
+import java.util.List;
+import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 
 @ConfigurationProperties("mail")
 public class MailConfig {
@@ -8,6 +11,9 @@ public class MailConfig {
     private String hostname;
     private int port;
     private String from;
+    private List<String> defaultRecipients;
+    private Map<String, Boolean> additionalHeaders;
+    private Credentials credentials;
 
     public String getHostname() {
         return hostname;
@@ -33,12 +39,39 @@ public class MailConfig {
         this.from = from;
     }
 
+    public List<String> getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(final List<String> defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public Map<String, Boolean> getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(final Map<String, Boolean> additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(final Credentials credentials) {
+        this.credentials = credentials;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ", defaultRecipients='" + defaultRecipients.toString() + '\'' +
+                ", additionalHeaders='" + additionalHeaders.toString() + '\'' +
+                ", credential='" + credentials.toString() + '\'' +
                 '}';
     }
 }
